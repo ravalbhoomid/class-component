df854e6ff1f1fa0b3c881b00ba2ace0fe0faefb0
bhoomi.raval <bhoomi.raval@seaflux.tech>
2023-03-11 12:03:01 +0530

    Initial Commit

---
+++ src/components/UserFinder.js
@@ -0,0 +1,81 @@
+import { Fragment, Component } from "react";
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
+// const DUMMY_USERS = [
+//   { id: "u1", name: "Max" },
+//   { id: "u2", name: "Manuel" },
+//   { id: "u3", name: "Julie" },
+// ];
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
+
+  componentDidMount() {
+    //send http request
+    this.setState({ filteredUsers: this.context.users });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+          <ErrorBoundary>
+            <Users users={this.state.filteredUsers} />
+          </ErrorBoundary>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+// useEffect(() => {}, [someDep])
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//         <Users users={filteredUsers} />
+//       </div>
+//     </Fragment>
+//   );
+// };
+
+export default UserFinder;
